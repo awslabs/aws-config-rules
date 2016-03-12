8d50c30b876dd3c530c119792527525546d3b2d2
Aaron Khoo <aaron.khoo@gmail.com>
2016-03-11 18:38:41 -0800

    Fixing password policy rule
    The rule was not calling put_evaluations at the end to actually store
    the compliance information.

--- python/password_policy_min_password_length_CIS_benchmark_1_9-periodic.py
+++ python/password_policy_min_password_length_CIS_benchmark_1_9-periodic.py
@@ -4,19 +4,35 @@
 # CIS Amazon Web Services Foundations Benchmark 1.9 Ensure IAM password policy requires minimum length of 14 or greater (Scored)
 #
 
-import boto3
+import boto3, json
 
 def lambda_handler(event, context):
     #Create AWS clients
     iam = boto3.client('iam')
     config = boto3.client('config')
-
+    
+    #Create current invokation info
+    invoking_event = json.loads(event['invokingEvent'])
+    compliance_value = 'NOT_APPLICABLE'
+    account_id = event['accountId']
+            
     #Get Account Password Policy
     response = iam.get_account_password_policy()
     pw_length = response['PasswordPolicy']['MinimumPasswordLength']
     
     #Evaluate Compliance
     if (pw_length < 14):
-        return 'NON_COMPLIANT'
+        compliance_value = 'NON_COMPLIANT'
     else:
-        return 'COMPLIANT'
+        compliance_value = 'COMPLIANT'
+        
+    response = config.put_evaluations(
+       Evaluations=[
+            {
+                'ComplianceResourceType': 'AWS::::Account',
+                'ComplianceResourceId': account_id,
+                'ComplianceType': compliance_value,
+                'OrderingTimestamp': invoking_event['notificationCreationTime']
+            },
+       ],
+       ResultToken=event['resultToken'])
