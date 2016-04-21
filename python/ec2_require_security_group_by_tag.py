9ec18647dcf0ba05adb3e24dc7664aff3e2c8f80
Aaron Khoo <aaron.khoo@gmail.com>
2016-04-21 14:08:25 -0700

    Updates based on review feedback
    1. Removed unnecessary variable
    2. Cleaned up a comment
    3. Rearranged some code

--- python/ec2_require_security_group_by_tag.py
+++ python/ec2_require_security_group_by_tag.py
@@ -19,31 +19,28 @@ import re
 def is_applicable(config_item, event):
     status = config_item['configurationItemStatus']
     event_left_scope = event['eventLeftScope']
-    test = ((status in ['OK', 'ResourceDiscovered']) and
-            event_left_scope == False)
-    return test
+    return ((status in ['OK', 'ResourceDiscovered']) and
+            (event_left_scope == False) and
+            (config_item['resourceType'] == 'AWS::EC2::Instance'))
 
 def evaluate_compliance(config_item, rule_parameters):
     # Initialize evaluation to 'not applicable', i.e. rule doesn't apply
     evaluation = 'NOT_APPLICABLE'
-    if (config_item['resourceType'] == 'AWS::EC2::Instance'):
-        configuration = config_item['configuration']
-        tags = configuration['tags']
-        foundTag = False
-        # If the config item is for an EC2 instance, then iterate through the tags for that instance
-        for tag in tags:
-            # Check if this is the name tag, and that it matches the provided regex value
-            reg = re.compile(rule_parameters['name'])
-            if (tag['key'] == 'Name') and (reg.match(tag['value']) != None):
-                # if so, initialize to 'non-compliant'
-                evaluation = 'NON_COMPLIANT'
-                secGroups = configuration['securityGroups']
-                # iterate through the security groups and see if the provided secGroup name is in the list. 
-                # if so, set compliance to 'compliant'
-                for secGroup in secGroups:
-                    if (secGroup['groupName'] == rule_parameters['secGroup']):
-                        evaluation = 'COMPLIANT'
-        
+    configuration = config_item['configuration']
+    tags = configuration['tags']
+    # If the config item is for an EC2 instance, then iterate through the tags for that instance
+    for tag in tags:
+        # Check if this is the 'Name' tag, and that it matches the provided regex value
+        reg = re.compile(rule_parameters['name'])
+        if (tag['key'] == 'Name') and (reg.match(tag['value']) != None):
+            # if so, initialize to 'non-compliant'
+            evaluation = 'NON_COMPLIANT'
+            secGroups = configuration['securityGroups']
+            # iterate through the security groups and see if the provided secGroup name is in the list. 
+            # if so, set compliance to 'compliant'
+            for secGroup in secGroups:
+                if (secGroup['groupName'] == rule_parameters['secGroup']):
+                    evaluation = 'COMPLIANT'
     return evaluation
 
 def lambda_handler(event, context):
