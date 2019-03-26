9e33b12498613138a51e9e44039bb72695b6ada0
S <chirgs@amazon.com>
2019-03-26 16:15:08 +0530

    update rdk version

--- python/EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK/EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK.py
+++ python/EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK/EBS_SNAPSHOT_PUBLIC_RESTORABLE_CHECK.py
@@ -31,9 +31,15 @@ Scenarios:
      Then: Return NON_COMPLIANT with Annotation containing SnapshotIDs
 '''
 import json
+import sys
 import datetime
 import boto3
 import botocore
+
+try:
+    import liblogging
+except ImportError:
+    pass
 
 ##############
 # Parameters #
@@ -45,12 +51,15 @@ DEFAULT_RESOURCE_TYPE = 'AWS::::Account'
 # Set to True to get the lambda to assume the Role attached on the Config Service (useful for cross-account).
 ASSUME_ROLE_MODE = False
 
+# Other parameters (no change needed)
+CONFIG_ROLE_TIMEOUT_SECONDS = 900
+
 #############
 # Main Code #
 #############
 
 
-# Function to generate evaluations for all non compliant resources
+# Function to get IDs for all non compliant resources
 def generate_snapshot_id_list(snapshots, event):
     snapshot_ids = []
     for snapshot in snapshots:
@@ -72,15 +81,19 @@ def get_public_snapshots(ec2_client, owner_id):
         if 'NextToken' in snapshots_result:
             next_token = snapshots_result['NextToken']
         else:
-            return(True, snapshots)
+            return snapshots
+
 
 def evaluate_compliance(event, configuration_item, valid_rule_parameters):
+    evaluations = []
     ec2_client = get_client('ec2', event)
     public_snapshots_result = get_public_snapshots(ec2_client, event['accountId'])
-    snapshot_ids = generate_snapshot_id_list(public_snapshots_result[1], event)
-    if snapshot_ids:
-        return build_evaluation(event['accountId'], 'NON_COMPLIANT', event, annotation='Public Amazon EBS Snapshots: {}'.format(snapshot_ids))
-    return build_evaluation(event['accountId'], 'COMPLIANT', event, annotation='No public Amazon EBS Snapshots')
+    if not public_snapshots_result:
+        return build_evaluation(event['accountId'], 'COMPLIANT', event)
+    snapshot_ids = generate_snapshot_id_list(public_snapshots_result, event)
+    for snapshot_id in snapshot_ids:
+        evaluations.append(build_evaluation(event['accountId'], 'NON_COMPLIANT', event, annotation='Public Amazon EBS Snapshot: {}'.format(snapshot_id)))
+    return evaluations
 
 
 def evaluate_parameters(rule_parameters):
@@ -106,10 +119,10 @@ def build_parameters_value_error_response(ex):
     Keyword arguments:
     ex -- Exception text
     """
-    return  build_error_response(internalErrorMessage="Parameter value is invalid",
-                                 internalErrorDetails="An ValueError was raised during the validation of the Parameter value",
-                                 customerErrorCode="InvalidParameterValueException",
-                                 customerErrorMessage=str(ex))
+    return  build_error_response(internal_error_message="Parameter value is invalid",
+                                 internal_error_details="An ValueError was raised during the validation of the Parameter value",
+                                 customer_error_code="InvalidParameterValueException",
+                                 customer_error_message=str(ex))
 
 # This gets the client after assuming the Config service role
 # either in the same AWS account or cross-account.
@@ -193,53 +206,57 @@ def get_configuration(resource_type, resource_id, configuration_capture_time):
         resourceId=resource_id,
         laterTime=configuration_capture_time,
         limit=1)
-    configurationItem = result['configurationItems'][0]
-    return convert_api_configuration(configurationItem)
+    configuration_item = result['configurationItems'][0]
+    return convert_api_configuration(configuration_item)
 
 # Convert from the API model to the original invocation model
-def convert_api_configuration(configurationItem):
-    for k, v in configurationItem.items():
+def convert_api_configuration(configuration_item):
+    for k, v in configuration_item.items():
         if isinstance(v, datetime.datetime):
-            configurationItem[k] = str(v)
-    configurationItem['awsAccountId'] = configurationItem['accountId']
-    configurationItem['ARN'] = configurationItem['arn']
-    configurationItem['configurationStateMd5Hash'] = configurationItem['configurationItemMD5Hash']
-    configurationItem['configurationItemVersion'] = configurationItem['version']
-    configurationItem['configuration'] = json.loads(configurationItem['configuration'])
-    if 'relationships' in configurationItem:
-        for i in range(len(configurationItem['relationships'])):
-            configurationItem['relationships'][i]['name'] = configurationItem['relationships'][i]['relationshipName']
-    return configurationItem
+            configuration_item[k] = str(v)
+    configuration_item['awsAccountId'] = configuration_item['accountId']
+    configuration_item['ARN'] = configuration_item['arn']
+    configuration_item['configurationStateMd5Hash'] = configuration_item['configurationItemMD5Hash']
+    configuration_item['configurationItemVersion'] = configuration_item['version']
+    configuration_item['configuration'] = json.loads(configuration_item['configuration'])
+    if 'relationships' in configuration_item:
+        for i in range(len(configuration_item['relationships'])):
+            configuration_item['relationships'][i]['name'] = configuration_item['relationships'][i]['relationshipName']
+    return configuration_item
 
 # Based on the type of message get the configuration item
 # either from configurationItem in the invoking event
 # or using the getResourceConfigHistiry API in getConfiguration function.
-def get_configuration_item(invokingEvent):
-    check_defined(invokingEvent, 'invokingEvent')
-    if is_oversized_changed_notification(invokingEvent['messageType']):
-        configurationItemSummary = check_defined(invokingEvent['configurationItemSummary'], 'configurationItemSummary')
-        return get_configuration(configurationItemSummary['resourceType'], configurationItemSummary['resourceId'], configurationItemSummary['configurationItemCaptureTime'])
-    elif is_scheduled_notification(invokingEvent['messageType']):
+def get_configuration_item(invoking_event):
+    check_defined(invoking_event, 'invokingEvent')
+    if is_oversized_changed_notification(invoking_event['messageType']):
+        configuration_item_summary = check_defined(invoking_event['configuration_item_summary'], 'configurationItemSummary')
+        return get_configuration(configuration_item_summary['resourceType'], configuration_item_summary['resourceId'], configuration_item_summary['configurationItemCaptureTime'])
+    elif is_scheduled_notification(invoking_event['messageType']):
         return None
-    return check_defined(invokingEvent['configurationItem'], 'configurationItem')
+    return check_defined(invoking_event['configurationItem'], 'configurationItem')
 
 # Check whether the resource has been deleted. If it has, then the evaluation is unnecessary.
-def is_applicable(configurationItem, event):
+def is_applicable(configuration_item, event):
     try:
-        check_defined(configurationItem, 'configurationItem')
+        check_defined(configuration_item, 'configurationItem')
         check_defined(event, 'event')
     except:
         return True
-    status = configurationItem['configurationItemStatus']
-    eventLeftScope = event['eventLeftScope']
+    status = configuration_item['configurationItemStatus']
+    event_left_scope = event['eventLeftScope']
     if status == 'ResourceDeleted':
         print("Resource Deleted, setting Compliance Status to NOT_APPLICABLE.")
-    return (status == 'OK' or status == 'ResourceDiscovered') and not eventLeftScope
+    return (status == 'OK' or status == 'ResourceDiscovered') and not event_left_scope
 
 def get_assume_role_credentials(role_arn):
     sts_client = boto3.client('sts')
     try:
-        assume_role_response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName="configLambdaExecution")
+        assume_role_response = sts_client.assume_role(RoleArn=role_arn,
+                                                      RoleSessionName="configLambdaExecution",
+                                                      DurationSeconds=CONFIG_ROLE_TIMEOUT_SECONDS)
+        if 'liblogging' in sys.modules:
+            liblogging.logSession(role_arn, assume_role_response)
         return assume_role_response['Credentials']
     except botocore.exceptions.ClientError as ex:
         # Scrub error message for any internal account info leaks
@@ -287,8 +304,10 @@ def clean_up_old_evaluations(latest_evaluations, event):
 
     return cleaned_evaluations + latest_evaluations
 
-# This decorates the lambda_handler in rule_code with the actual PutEvaluation call
 def lambda_handler(event, context):
+    if 'liblogging' in sys.modules:
+        liblogging.logEvent(event)
+
     global AWS_CONFIG_CLIENT
 
     #print(event)
@@ -354,17 +373,17 @@ def lambda_handler(event, context):
         evaluations.append(build_evaluation_from_config_item(configuration_item, 'NOT_APPLICABLE'))
 
     # Put together the request that reports the evaluation status
-    resultToken = event['resultToken']
-    testMode = False
-    if resultToken == 'TESTMODE':
+    result_token = event['resultToken']
+    test_mode = False
+    if result_token == 'TESTMODE':
         # Used solely for RDK test to skip actual put_evaluation API call
-        testMode = True
+        test_mode = True
 
     # Invoke the Config API to report the result of the evaluation
     evaluation_copy = []
     evaluation_copy = evaluations[:]
-    while(evaluation_copy):
-        AWS_CONFIG_CLIENT.put_evaluations(Evaluations=evaluation_copy[:100], ResultToken=resultToken, TestMode=testMode)
+    while evaluation_copy:
+        AWS_CONFIG_CLIENT.put_evaluations(Evaluations=evaluation_copy[:100], ResultToken=result_token, TestMode=test_mode)
         del evaluation_copy[:100]
 
     # Used solely for RDK test to be able to test Lambda function
@@ -374,15 +393,15 @@ def is_internal_error(exception):
     return ((not isinstance(exception, botocore.exceptions.ClientError)) or exception.response['Error']['Code'].startswith('5')
             or 'InternalError' in exception.response['Error']['Code'] or 'ServiceError' in exception.response['Error']['Code'])
 
-def build_internal_error_response(internalErrorMessage, internalErrorDetails=None):
-    return build_error_response(internalErrorMessage, internalErrorDetails, 'InternalError', 'InternalError')
+def build_internal_error_response(internal_error_message, internal_error_details=None):
+    return build_error_response(internal_error_message, internal_error_details, 'InternalError', 'InternalError')
 
-def build_error_response(internalErrorMessage, internalErrorDetails=None, customerErrorCode=None, customerErrorMessage=None):
+def build_error_response(internal_error_message, internal_error_details=None, customer_error_code=None, customer_error_message=None):
     error_response = {
-        'internalErrorMessage': internalErrorMessage,
-        'internalErrorDetails': internalErrorDetails,
-        'customerErrorMessage': customerErrorMessage,
-        'customerErrorCode': customerErrorCode
+        'internalErrorMessage': internal_error_message,
+        'internalErrorDetails': internal_error_details,
+        'customerErrorMessage': customer_error_message,
+        'customerErrorCode': customer_error_code
     }
     print(error_response)
     return error_response
